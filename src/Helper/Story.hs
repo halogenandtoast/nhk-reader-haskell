a4a3fbe811a0be9ed18832b960ff546ac59cde6b
Matthew Mongeau <halogenandtoast@gmail.com>
2018-06-21 22:10:53 +0900

    sanitizeHelper

--- src/Helper/Story.hs
+++ src/Helper/Story.hs
@@ -4,6 +4,13 @@ module Helper.Story where
 
 import Import
 import Hledger.Utils.Regex
+import qualified Data.Text as DT
 
 newsStoryTitleWithBr :: NewsStory -> Text
 newsStoryTitleWithBr = pack . regexReplace "\12288" "<br>" . unpack . newsStoryTitleWithRuby
+
+sanitizedBody :: NewsStory -> Maybe Text
+sanitizedBody newsStory = DT.splitOn "</div>" <$> newsStoryBody newsStory >>= safeHead
+  where
+    safeHead (x:_) = Just x
+    safeHead _ = Nothing
